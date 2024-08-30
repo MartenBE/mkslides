39e304231d898175fe4d8646a56bf075c49dcee5
Martijn Saelens <martijn.saelens@protonmail.com>
2024-08-30 17:38:16 +0200

    First working draft of static generator

--- src/main.py
+++ src/main.py
@@ -7,22 +7,38 @@ from pathlib import Path
 
 ################################################################################
 
-MD_IMG_REGEX = re.compile(
+MD_IMAGE_REGEX = re.compile(
     r"""
-    !                            # Start of the image
-    \[.*?\]                      # Alt text
-    \((?P<location>.*?)\)        # Image location
+    !                           # Start of the image
+    \[.*?\]                     # Alt text
+    \((?P<location>.*?)\)       # Image location
     """,
     re.VERBOSE,
 )
-HTML_IMG_REGEX = re.compile(
+HTML_IMAGE_REGEX = re.compile(
     r"""
-    <img                       # Start of the image
-    .+?                        # Any attributes
-    src=                       # src attribute
+    <img                        # Start of the image
+    .+?                         # Any attributes
+    src=                        # src attribute
         (?P<delimiter>['\"])    # Delimiter
-        (?P<location>.*?)       # Image location
+        (?P<location>.+?)       # Image location
         (?P=delimiter)          # Delimiter
+    .*?                         # Any attributes
+    >                           # End of the image
+    """,
+    re.VERBOSE,
+)
+
+HTML_BACKGROUND_IMAGE_REGEX = re.compile(
+    r"""
+    <!--                        # Start of the comment
+    .*?                         # Any content
+    data-background-image=      # data-background-image attribute
+        (?P<delimiter>['\"])    # Delimiter
+        (?P<location>.+?)       # Image location
+        (?P=delimiter)          # Delimiter
+    .*?                         # Any content
+    -->                         # End of the comment
     """,
     re.VERBOSE,
 )
@@ -30,28 +46,8 @@ HTML_IMG_REGEX = re.compile(
 ################################################################################
 
 
-def detect_images(content):
-    images = set()
-
-    for m in MD_IMG_REGEX.finditer(content):
-        images.add(m.group("location"))
-
-    for m in HTML_IMG_REGEX.finditer(content):
-        images.add(m.group("location"))
-
-    print(images)
-
-
-def process_md_file(file):
-    print(f'Processing "{file}"')
-    with open(file, "r") as f:
-        markdown_content = f.read()
-
-        # Copy the markdown content to template
-
-        # Copy images
-
-        images = detect_images(markdown_content)
+def assert_path_exists(path: Path):
+    assert path.exists(), f'"{path.absolute()}" does not exist'
 
 
 ################################################################################
@@ -65,28 +61,96 @@ parser.add_argument(
     help="Path to the markdown file(s), or the directory containing markdown files",
 )
 parser.add_argument(
-    "-o", "--output", type=str, help="Path to the output directory", default="html"
+    "-o", "--output", type=str, help="The output directory", default="html"
 )
 parser.add_argument(
     "-c", "--config", type=str, help="Path to the config file", default=".revealpy.yml"
 )
 args = parser.parse_args()
 
+# Input path
 
-output_path = Path(args.output)
-print(f"Output path: {output_path}")
-if output_path.exists():
-    print("Output path exists. Deleting and recreating ...")
-    shutil.rmtree(output_path)
-output_path.mkdir()
-shutil.copytree("assets/templates/revealjs", output_path / "assets/revealjs")
+input_path = Path(args.files)
+print(f'Input path: "{input_path.absolute()}"')
+assert_path_exists(input_path)
 
+# Output directory
+
+output_directory = Path(args.output)
+print(f'Output directory: "{output_directory.absolute()}"')
+
+if output_directory.exists():
+    shutil.rmtree(output_directory)
+    print("Output directory already exists: deleted")
+
+output_directory.mkdir()
+print(f"Output directory created.")
+
+# Reveal.js
+
+revealjs_path = Path("assets/reveal.js-master")
+result_revealjs_path = output_directory / "assets" / "reveal-js"
+shutil.copytree(revealjs_path, result_revealjs_path)
+print(f'\tCopied "{revealjs_path.absolute()}" to "{result_revealjs_path.absolute()}"')
+
+# Jinja2
 
 environment = jinja2.Environment()
 environment.loader = jinja2.FileSystemLoader("assets/templates")
-slideshow_template = environment.get_template("slideshow.html")
-# index_template = environment.get_template("index.html")
+slideshow_template = environment.get_template("slideshow.html.jinja")
 
-# input_path = Path(args.files)
-# for md_file in input_path.glob("**/*.md"):
-#     process_md_file(md_file)
+# Process markdown files
+
+index = []
+
+for md_file in input_path.glob("**/*.md"):
+
+    # Generate the markup from markdown
+
+    markdown = md_file.read_text()
+
+    print(md_file)
+    print(result_revealjs_path)
+
+    markup = slideshow_template.render(
+        revealjs_path=result_revealjs_path.relative_to(output_directory),
+        markdown=markdown,
+    )
+
+    result_markup_path = output_directory / md_file.relative_to(input_path)
+    result_markup_path = result_markup_path.with_suffix(".html")
+    result_markup_path.parent.mkdir(parents=True, exist_ok=True)
+    result_markup_path.write_text(markup)
+    print(f'Transformed "{md_file.absolute()}" into "{result_markup_path.absolute()}"')
+
+    # TODO: fix debug folder
+
+    # Copy images
+
+    for regex in [MD_IMAGE_REGEX, HTML_IMAGE_REGEX, HTML_BACKGROUND_IMAGE_REGEX]:
+        for m in regex.finditer(markdown):
+            image = Path(md_file.parent, m.group("location"))
+            assert_path_exists(image)
+
+            result_image_path = output_directory / image.relative_to(input_path)
+            result_image_path.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy(image, result_image_path)
+            print(
+                f'\t\tCopied "{image.absolute()}" to "{result_image_path.absolute()}"'
+            )
+
+    # Index
+
+    index.append(result_markup_path.relative_to(output_directory))
+
+    print()
+    print()
+    print()
+
+# Generate the index
+
+print(index)
+index_template = environment.get_template("index.html.jinja")
+index_path = output_directory / "index.html"
+index_path.write_text(index_template.render(index=index))
+print(f'Generated index: "{index_path.absolute()}"')
