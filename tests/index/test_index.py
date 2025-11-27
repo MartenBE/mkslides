e4261a5a05877db2a16f3cc0641be66160e40fd9
Martijn Saelens <martijn.saelens@protonmail.com>
2025-11-27 10:58:43 +0100

    Added unit tests

--- tests/index/test_index.py
+++ tests/index/test_index.py
@@ -4,7 +4,11 @@
 
 from typing import Any
 
-from tests.utils import assert_html_contains, run_build_strict
+from tests.utils import (
+    assert_html_contains,
+    assert_html_does_not_contain,
+    run_build_strict,
+)
 
 
 def test_index_title(setup_paths: Any) -> None:
@@ -17,4 +21,27 @@ def test_index_title(setup_paths: Any) -> None:
     assert_html_contains(
         output_path / "index.html",
         "<h1>Lorem ipsum</h1>",
+    )
+
+
+def test_index_banner(setup_paths: Any) -> None:
+    cwd, output_path = setup_paths
+    input_path = cwd / "index" / "slides"
+    run_build_strict(cwd, input_path, output_path, None)
+
+    assert_html_contains(
+        output_path / "index.html",
+        'Documentation built with <a href="https://martenbe.github.io/mkslides/" target="_blank">MkSlides</a>.',
+    )
+
+
+def test_index_no_banner(setup_paths: Any) -> None:
+    cwd, output_path = setup_paths
+    input_path = cwd / "index" / "slides"
+    config_path = cwd / "index" / "index_no_banner-config.yml"
+    run_build_strict(cwd, input_path, output_path, config_path)
+
+    assert_html_does_not_contain(
+        output_path / "index.html",
+        'Documentation built with <a href="https://martenbe.github.io/mkslides/" target="_blank">MkSlides</a>.',
     )
